a7b706ac8192e5ea302cab1b361973b1e806cf6e
rgarza7@gmail.com Garza <rgarza7@gmail.com>
2018-05-28 19:38:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(instrument, musician) {
   var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var emptyArray = [];
+  var i = [];
+    for ()
   
 }
