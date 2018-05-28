e06dd180705449677f1f9d58173ce43aacbbbceb
rgarza7@gmail.com Garza <rgarza7@gmail.com>
2018-05-28 19:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ function theBeatlesPlay(instrument, musician) {
       for (j = 0; j < instruments.length; j++) {
         instrumentPlayed.push(musicians[i] + " plays " + instruments[j]);
     }
+  }
+}
