94298714ad3c4757b7517271e28c3412e883d247
rgarza7@gmail.com Garza <rgarza7@gmail.com>
2018-05-28 20:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function johnLennonFacts(facts) {
   var emptyArray = [];
   var i = 0;
   while (i < facts.length) {
-    emptyArray.push(fact[i] + !!!)
+    emptyArray.push(fact[i] + !!!);
+    i++;
   }
   
 }
